17f52f49ad0606a533f4ee92240f91d0ae006f19
kgryte <kgryte@gmail.com>
2016-02-27 13:25:38 -0800

    Fix variable names

--- test/test.js
+++ test/test.js
@@ -4,17 +4,17 @@
 
 var tape = require( 'tape' );
 var pow = require( 'math-power' );
-var MAX_UINT32 = require( './../lib' );
+var MAX_UINT16 = require( './../lib' );
 
 
 // TESTS //
 
 tape( 'main export is a number', function test( t ) {
-	t.equal( typeof MAX_UINT32, 'number', 'main export is a number' );
+	t.equal( typeof MAX_UINT16, 'number', 'main export is a number' );
 	t.end();
 });
 
 tape( 'the exported value is 2**16-1', function test( t ) {
-	t.equal( MAX_UINT32, pow(2,16)-1, 'returns 2**16-1' );
+	t.equal( MAX_UINT16, pow(2,16)-1, 'returns 2**16-1' );
 	t.end();
 });
